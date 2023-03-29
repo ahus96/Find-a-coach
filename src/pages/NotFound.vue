33d63944e370106e30e8aac9950b2779d8a21a60
ahus96 <36034401+ahus96@users.noreply.github.com>
2023-03-29 16:13:11 +0100

    Add files via upload

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        this page cannot be found check out our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
+
